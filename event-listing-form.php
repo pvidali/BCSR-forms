dfd95068b4d8dc8c45609f5b3bea93b2836a7561
MC (laptop) <mchameides@ITSs-MacBook-Pro-4.local>
2015-09-17 14:41:23 -0400

    initial import

---
+++ event-listing-form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Event Listing Request Form</title>
+</head>
+
+<body>
+
+<?php
+	// echo "HELLO";
+	include("https://projectfork.simons-rock.edu/index.php/event-listing-form");
+
+?>
+</body>
+</html>
